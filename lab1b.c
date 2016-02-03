1bee063e52fe0490219b921bfc851b2115e74186
csrobot <csrobot@os302-2.uml.edu>
2016-02-03 14:35:28 -0500

    Finished lab1. Merged line code with bump sensor code

--- lab1b.c
+++ lab1b.c
@@ -1,25 +1,69 @@
 // Created on Fri Jan 29 2016
 
-int main() 
+int movingLeft = 1;
+int crossedLine = 0;
+const int black = 800;		
+const int white = 400;
+	
+void left() 
+{			
+	movingLeft = 1;
+	crossedLine = 0;
+	move_at_velocity(0, 500); // Right wheel
+	move_at_velocity(3, -50); // Left wheel
+	return;
+}
+	
+void right()
+{	
+	movingLeft = 0;
+	crossedLine = 0;
+	move_at_velocity(0, -50); // Right wheel
+	move_at_velocity(3, 500); // Left wheel
+	return;
+}
+
+void loop() 
 {
-	printf("Hello, World!\n");
+	
 	while(1)
 	{
-		int colorSeen = analog10(0);
+		int isPressed = digital(15);
 		
-		if(colorSeen < 500)
+		if(isPressed == 1)
 		{
-			move_at_velocity(0,200);
-			move_at_velocity(3,400);
-			//msleep();
+			move_at_velocity(0,0);
+			move_at_velocity(3,0);
+			msleep(2000);
+			left();
 		}
-		else
+		
+		printf("%d",isPressed);
+		
+		int color = analog10(0);
+		printf("%d\n",color);
+		if (crossedLine == 0 && color >= black)
 		{
-			move_at_velocity(0,400);
-			move_at_velocity(3,200);
+			crossedLine = 1;
+		} else if (crossedLine == 1 && color < white ) {
+			if (movingLeft) {
+				right();
+			} else {
+				left();
+			}
 		}
+		
 
 	}
+	return;
+	
+}
+
+int main() 
+{
+	left();
+	loop();
 	
 	return 0;
 }
+
